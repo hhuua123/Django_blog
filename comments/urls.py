2fb677f5db1e35e294172507d9f455c4bce9c1b4
hhuua123 <911981009@qq.com>
2018-05-30 12:46:25 +0800

    Merge branch 'dev' of github.com:hhuua123/Django_blog into dev
    # Conflicts:
    #	.gitignore

--- comments/urls.py
+++ comments/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'comments'
 urlpatterns = [
-    path(r'^comment/post/(?P<post_pk>[0-9]+)/$', views.post_comment, name='post_comment'),
+    path('comment/post/<int: post_pk>/', views.post_comment, name='post_comment'),
 ]
