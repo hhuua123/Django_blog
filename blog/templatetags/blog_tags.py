eef57517ab4d3742ccfc656cdd79c1008e276db8
hhuua123 <911981009@qq.com>
2018-05-31 15:07:35 +0800

    修改整理注释,修改部分js

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -4,21 +4,26 @@ from django.db.models.aggregates import Count
 
 register = template.Library()
 
+
 @register.simple_tag
 def get_recent_posts(num=5):
     return Post.objects.all().order_by('-created_time')[:num]
+
 
 @register.simple_tag
 def archives():
     return Post.objects.dates('created_time', 'month', order='DESC')
 
+
 @register.simple_tag
 def get_categories():
     return Category.objects.all()
 
+
 @register.simple_tag
 def get_tags():
     return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
 
 # 获取分类文章数
 @register.simple_tag
